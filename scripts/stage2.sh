a48c6e2bea33a094680c40603383239e1f52e9cb
Markov Andrei <me@markovav.ru>
2024-04-20 19:03:17 +0300

    Fixed wrong stage script

--- scripts/stage2.sh
+++ scripts/stage2.sh
@@ -12,3 +12,9 @@ hdfs dfs -put output/*.avsc project/warehouse/avsc
 
 beeline -u jdbc:hive2://hadoop-03.uni.innopolis.ru:10001 -n team21 -p $PASSWORD -f sql/db.hql > output/hive_results.txt 2> /dev/null
 beeline -u jdbc:hive2://hadoop-03.uni.innopolis.ru:10001 -n team21 -p $PASSWORD -f sql/optimizations.hql > /dev/null 2> /dev/null
+
+beeline -u jdbc:hive2://hadoop-03.uni.innopolis.ru:10001 -n team21 -p $password -f sql/q1.hql --hiveconf hive.resultset.use.unique.column.names=false > output/q1.csv
+beeline -u jdbc:hive2://hadoop-03.uni.innopolis.ru:10001 -n team21 -p $password -f sql/q2.hql --hiveconf hive.resultset.use.unique.column.names=false > output/q2.csv
+beeline -u jdbc:hive2://hadoop-03.uni.innopolis.ru:10001 -n team21 -p $password -f sql/q3.hql --hiveconf hive.resultset.use.unique.column.names=false > output/q3.csv
+beeline -u jdbc:hive2://hadoop-03.uni.innopolis.ru:10001 -n team21 -p $password -f sql/q4.hql --hiveconf hive.resultset.use.unique.column.names=false > output/q4.csv
+beeline -u jdbc:hive2://hadoop-03.uni.innopolis.ru:10001 -n team21 -p $password -f sql/q5.hql --hiveconf hive.resultset.use.unique.column.names=false > output/q5.csv
--- scripts/stage3.sh
+++ scripts/stage3.sh
@@ -4,9 +4,3 @@
 PASSWORD=$(head -n 1 secrets/.hive.pass)
 
 set -xe
-
-beeline -u jdbc:hive2://hadoop-03.uni.innopolis.ru:10001 -n team21 -p $password -f sql/q1.hql --hiveconf hive.resultset.use.unique.column.names=false > output/q1.csv
-beeline -u jdbc:hive2://hadoop-03.uni.innopolis.ru:10001 -n team21 -p $password -f sql/q2.hql --hiveconf hive.resultset.use.unique.column.names=false > output/q2.csv
-beeline -u jdbc:hive2://hadoop-03.uni.innopolis.ru:10001 -n team21 -p $password -f sql/q3.hql --hiveconf hive.resultset.use.unique.column.names=false > output/q3.csv
-beeline -u jdbc:hive2://hadoop-03.uni.innopolis.ru:10001 -n team21 -p $password -f sql/q4.hql --hiveconf hive.resultset.use.unique.column.names=false > output/q4.csv
-beeline -u jdbc:hive2://hadoop-03.uni.innopolis.ru:10001 -n team21 -p $password -f sql/q5.hql --hiveconf hive.resultset.use.unique.column.names=false > output/q5.csv
