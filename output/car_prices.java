12e5a9c0c0e63c4eb2fd200e8cd8ea74d2c691f8
Andrei Markov <me@markovav.ru>
2024-04-21 16:35:18 +0300

    New outputs

--- output/car_prices.java
+++ output/car_prices.java
@@ -2,7 +2,7 @@
 // WARNING: This class is AUTO-GENERATED. Modify at your own risk.
 //
 // Debug information:
-// Generated date: Sun Apr 14 20:00:49 MSK 2024
+// Generated date: Sun Apr 21 16:30:14 MSK 2024
 // For connector: org.apache.sqoop.manager.PostgresqlManager
 import org.apache.hadoop.io.BytesWritable;
 import org.apache.hadoop.io.Text;
--- scripts/stage2.sh
+++ scripts/stage2.sh
@@ -13,7 +13,7 @@ hdfs dfs -put output/*.avsc project/warehouse/avsc
 beeline -u jdbc:hive2://hadoop-03.uni.innopolis.ru:10001 -n team21 -p $PASSWORD -f sql/db.hql > output/hive_results.txt 2> /dev/null
 beeline -u jdbc:hive2://hadoop-03.uni.innopolis.ru:10001 -n team21 -p $PASSWORD -f sql/optimizations.hql > /dev/null 2> /dev/null
 
-for (i = 1; i <= 5; i++)
+for ((i = 1; i <= 5; i++))
 do
 beeline -u jdbc:hive2://hadoop-03.uni.innopolis.ru:10001 -n team21 -p $PASSWORD -f sql/q$i.hql --hiveconf hive.resultset.use.unique.column.names=false > output/q$i.csv 2> /dev/null
 done
