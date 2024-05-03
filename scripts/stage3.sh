2c28f9fd7d3b0b7f30472d4ca59d1178dc14868e
Andrei Markov <me@markovav.ru>
2024-05-03 13:53:09 +0300

    Added outputs of stage-3

--- scripts/stage3.sh
+++ scripts/stage3.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
-# Reading secrets
-PASSWORD=$(head -n 1 secrets/.hive.pass)
-
 set -xe
+
+spark-submit --master yarn scripts/model.py
+
+# Split large train.json file into 2 parts
+awk '{ print > ( "data/train" (NR>(n/2) ? 2 : 1) ".json" ) }' n="$(wc -l <data/train.json)" data/train.json
+rm data/train.json
